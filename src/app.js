d6455c47ff180d2176a47595006e18181222af95
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2023-01-08 13:26:39 -0300

    alguns bonus inseridos

--- src/app.js
+++ src/app.js
@@ -22,44 +22,63 @@ function avatarForTweet(tweet) {
     return obj;
 }
 
-app.get("/tweets", (req, res) => {
+app.get("/tweets/:id?", (req, res) => {
 
     const everyTweets = tweets.map((tweet) => avatarForTweet(tweet));
 
-    res.send(everyTweets.slice(-10,everyTweets.length));
+    const USERNAME = req.params.id;
+    if(USERNAME)res.status(200).send(tweets.filter((tweet) => (tweet.username === USERNAME) && avatarForTweet(tweet)))
+
+    if(tweets.length === 0) res.status(200).send(tweets)
+
+    res.status(200).send(everyTweets.slice(-10,everyTweets.length));
 })
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
 
+
+
     if (!username || !avatar) res.status(400).send("object invalid");
+    if (Number(username)) res.status(400).send("username is not a string");
+    if (Number(avatar)) res.status(400).send("url is not a string");
 
     users.push({
         username: username,
         avatar: avatar
     });
 
-    res.status(200).send("OK");
+    res.status(201).send("OK");
 });
 
 app.post("/tweets", (req, res) => {
 
     const { username, tweet } = req.body;
-    const existUser = users.find(user => user.username == username)
+    const existUser = users.find(user => user.username == username);
 
-    console.log(req.body)
-    console.log(existUser)
+    const head = req.headers
+    const existUserHeaders = users.find(user => user.username == head.username)
+
+    if(head.username){
+        if(!existUserHeaders)res.status(401).send("user not found");
+        tweets.push({
+            username: head.username,
+            tweet: tweet
+        });
+    
+        res.status(201).send("OK");
+    }
 
     if (!username || !tweet) res.status(400).send("object invalid");
-
     if (!existUser) res.status(401).send("user not found");
+    if (Number(tweet)) res.status(400).send("tweet is not a string");
 
     tweets.push({
         username: username,
         tweet: tweet
-    })
+    });
 
-    res.send("OK")
+    res.status(201).send("OK");
 })
 
 app.listen(PORT);
